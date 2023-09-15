3ad2acbee2104ed848e6317fac3f7225e534a2fb
Julio Rodriguez <juliodevelops@gmail.com>
2023-09-15 12:31:34 -0400

    Annotate confusing parts of ByteStram/PrintStream

--- src/test/java/Pets/PetSurveyTest.java
+++ src/test/java/Pets/PetSurveyTest.java
@@ -15,15 +15,20 @@ class PetSurveyTest {
     void displayOptions() {
         String[] options = {"1. Dog", "2. Cat", "3. Rodent"};
 
+        // Create ByeArrrayOutputStream to store print that won't actually go to the console
         ByteArrayOutputStream consoleLog = new ByteArrayOutputStream();
-        PrintStream actualOut = System.out;
 
+//        PrintStream actualOut = System.out;
+
+        // Capture console out/direct the SOut to the consoleLog ByteStream
         System.setOut(new PrintStream(consoleLog));
+
 
         PetSurvey.displayOptions(options);
 
 
         for(String option : options){
+            // Convert ByteArray to String - check that it contains the option string
             assertTrue(consoleLog.toString().contains(option));
         }
 
