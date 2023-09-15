59de7d29cf6af368e2be5c85f38e722fb8790df4
Julio Rodriguez <juliodevelops@gmail.com>
2023-09-15 12:09:09 -0400

    Add PrintStream to properly test

--- src/test/java/Pets/PetSurveyTest.java
+++ src/test/java/Pets/PetSurveyTest.java
@@ -3,17 +3,25 @@ package Pets;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PetSurveyTest {
 
-    ByteArrayOutputStream consoleLog = new ByteArrayOutputStream();
+
+
     @Test
     void displayOptions() {
         String[] options = {"1. Dog", "2. Cat", "3. Rodent"};
 
+        ByteArrayOutputStream consoleLog = new ByteArrayOutputStream();
+        PrintStream actualOut = System.out;
+
+        System.setOut(new PrintStream(consoleLog));
+
         PetSurvey.displayOptions(options);
+
 
         for(String option : options){
             assertTrue(consoleLog.toString().contains(option));
